fff079740a81bfccaf8593ee36dfeaacb0e1c91e
marcusroberto02 <58505443+marcusroberto02@users.noreply.github.com>
2024-12-27 13:51:09 +0100

    revert back to original version cc_cbm.sh

--- script_dir/CBM/cc_cbm.sh
+++ script_dir/CBM/cc_cbm.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-#nvidia-smi
-ROOT=Deep-Learning-Project-CBM
-conda init
-conda activate denv
+nvidia-smi
+source activate ~/concept-driven-continual-learning/conda_env/merge_env
 echo "python training"
 con_method=c1
 MODEL=resnet18
@@ -14,9 +12,9 @@ ewc_lambda=0.2
 spar_lam=0
 mem=150
 nonlinear=True
-pretrain=none
+pretrain=True
 dif=False
-download_data=~/${ROOT}/download_data
+download_data=~/concept-driven-continual-learning/download_data
 
 task_num=5
 
@@ -31,8 +29,8 @@ SAVE_DIR=~/result/cc_cbm/
 for ((i=0; i<$task_num; i++));
 do
 DATASET=${dataset}_task_${i}_${task_num}_${SEED}
-concept_path=~/${ROOT}/sandbox-lf-cbm/data/concept_sets/${dataset}_task_${i}_${task_num}_${SEED}_filtered.txt
-python ~/${ROOT}/sandbox-lf-cbm/train_cbm.py --nonlinear ${nonlinear} --seed ${SEED} --gamma ${gamma} --solver ${solver} --lam ${spar_lam} --concept_method ${con_method} --dataset ${DATASET} --concept_set ${concept_path} --save_dir ${SAVE_DIR} --freeze_wc ${freeze} --normalize_wf ${nor_wf} 
+concept_path=~/concept-driven-continual-learning/sandbox-lf-cbm/data/concept_sets/${dataset}_task_${i}_${task_num}_${SEED}_filtered.txt
+python ~/concept-driven-continual-learning/sandbox-lf-cbm/train_cbm.py --nonlinear ${nonlinear} --seed ${SEED} --gamma ${gamma} --solver ${solver} --lam ${spar_lam} --concept_method ${con_method} --dataset ${DATASET} --concept_set ${concept_path} --save_dir ${SAVE_DIR} --freeze_wc ${freeze} --normalize_wf ${nor_wf} 
 done
 
-python ~/${ROOT}/sandbox-lf-cbm/evaluate_cbm.py --task_num ${task_num} --dataset ${dataset} --seed ${SEED} --save_dir ${SAVE_DIR}  --nonlinear ${nonlinear}
+python ~/concept-driven-continual-learning/sandbox-lf-cbm/evaluate_cbm.py --task_num ${task_num} --dataset ${dataset} --seed ${SEED} --save_dir ${SAVE_DIR}  --nonlinear ${nonlinear}
